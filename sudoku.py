e0b170a6d40cde34bbe5272ddde0de5ee1297c18
Khushi Chaudhari <khushi.chaudhari@gmail.com>
2023-08-07 22:42:54 -0700

    added new funct

--- sudoku.py
+++ sudoku.py
@@ -1,22 +1,31 @@
 import random
 import numpy as np
 
+def is_it_there(arr: list, value: int):
+    boole = False
+    for item in arr:
+        for items in item:
+            if value == items:
+                boole = True
+    return boole
+
 def solution():
-    # rows = 3
-    # columns = 3
+    rows = 3
+    columns = 3
 
     oned_array = np.arange(1,10)
     oned_array = np.random.choice(oned_array, size=9, replace=False)
     box1 = oned_array.reshape((3,3))
 
-    box2 = []
+    box2 = [[0]*columns]*rows
     box3 = []
     box4 = []
 
     for i,items in enumerate(box2):
         for j,item in enumerate(items):
             num = random.randint(1,9)
-            while num in box2 or num in box1[i]:
+            
+            while is_it_there(box2,num) == True or is_it_there(box1[i],num):
                 print(num)
                 num = random.randint(1,9)
             box2[i][j] = num
