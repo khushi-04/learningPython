14a9d96e5ea75e0a2f3b69c6195f368ed776a014
Khushi Chaudhari <khushi.chaudhari@gmail.com>
2023-08-08 14:08:03 -0700

    mini version sudoku works

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # learningPython
-Python Programming MOOC 2023
+Self learning python with small projects
+
+Project 1: Sudoku Game
+    What I'm exploring:
+        - lists
+        - 2D Arrays
+        - numpy applications
+        - randomization
+    Game description:
+        This is a sudoku game in progress. I included nine 3x3 boxes that all correspond with each other and each new game will include a new random set of numbers in the game. It's played in the terminal. 
+    Progress so far:
+        I'm still working on how to actually generate the boxes using numpy and randomization. I've been able to generate 4 boxes with random numbers that follow the sudoku rules so far. They are also formatted in the way that I want. Now I just have to do it for the rest of the boxes, which is where it might get a little complicated.    
+    What did I learn:
+        - The random library is helpful for generating any types of random numbers for any case.
+        - The numpy library is very useful for manipulating and generating 2D and 3D arrays. It has a variety of functions that allow you to change the way an array looks and is structured a lot easier than just using lists. 
--- sudoku.py
+++ sudoku.py
@@ -1,41 +1,72 @@
 import random
 import numpy as np
 
-def is_it_there(arr: list, value: int):
+def is_it_there(arr, value: int):
     boole = False
     for item in arr:
-        for items in item:
-            if value == items:
-                boole = True
+        if value == item:
+            boole = True
     return boole
 
 def solution():
     rows = 3
     columns = 3
-
+    # initializing box1
     oned_array = np.arange(1,10)
     oned_array = np.random.choice(oned_array, size=9, replace=False)
     box1 = oned_array.reshape((3,3))
 
-    box2 = [[0]*columns]*rows
-    box3 = []
-    box4 = []
+    box2 = [0,0,0,0,0,0,0,0,0]
+    box3 = [0,0,0,0,0,0,0,0,0]
+    box4 = [0,0,0,0,0,0,0,0,0]
 
+    # initializing box2
+    trackindex = 0
     for i,items in enumerate(box2):
-        for j,item in enumerate(items):
+        num = random.randint(1,9)
+        while is_it_there(box2,num) == True or num in box1[trackindex,:]:
             num = random.randint(1,9)
-            
-            while is_it_there(box2,num) == True or is_it_there(box1[i],num):
-                print(num)
-                num = random.randint(1,9)
-            box2[i][j] = num
+        box2[i] = num
+        if i == 2 or i == 5:
+            trackindex+=1
+    box2 = np.array(box2)
+    box2 = box2.reshape((3,3))
+
+    # initializing box3
+    trackindex = 0
+    for i,items in enumerate(box3):
+        num = random.randint(1,9)
+        while is_it_there(box3,num) == True or num in box1[:,trackindex]:
+            num = random.randint(1,9)
+        box3[i] = num
+        if i == 2 or i == 5:
+            trackindex = 0
+        else:
+            trackindex+=1
+    box3 = np.array(box3)
+    box3 = box3.reshape((3,3))
+
+    trackrows = 0
+    trackcols = 0
+    for i,items in enumerate(box4):
+        num = random.randint(1,9)
+        while is_it_there(box4,num) == True or num in box2[:,trackcols] or num in box3[trackrows,:]:
+            num = random.randint(1,9)
+        box4[i] = num
+        if i == 2 or i == 5:
+            trackrows+= 1
+            trackcols = 0
+        else:
+            trackcols+=1
+    box4 = np.array(box4)
+    box4 = box4.reshape((3,3))
+
+    print(np.hstack((box1, box2)))
+    print(np.hstack((box3,box4)))
     print(box1)
     print(box2)
-
+    print(box3)
+    print(box4)
 
 if __name__ == "__main__":
-    print("test")
     solution()
-
-
-
