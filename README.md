ae8eadbf2d0676fcdf943f60915218e52567dc15
Khushi Chaudhari <khushi.chaudhari@gmail.com>
2023-08-09 16:38:23 -0700

    working solution function

--- README.md
+++ README.md
@@ -13,7 +13,12 @@
 This is a sudoku game in progress. I included nine 3x3 boxes that all correspond with each other and each new game will include a new random set of numbers in the game. It's played in the terminal. 
 
 #### Progress so far:
-I'm still working on how to actually generate the boxes using Numpy and randomization. I've been able to generate 4 boxes with random numbers that follow the sudoku rules so far. They are also formatted in the way that I want. Now I have to do it for the rest of the boxes, which is where it might get a little complicated.    
+8/5: I'm still working on how to actually generate the boxes using Numpy and randomization. It's been really confusing since I can't get the numbers to randomize and they repeat between the rows. 
+
+8/7: I've been able to generate 4 boxes with random numbers that follow the sudoku rules so far. They are also formatted in the way that I want. Now I have to do it for the rest of the boxes, which is where it might get a little complicated.
+
+8/9: One of the frustrating things I've been dealing with is my code not running properly in the terminal. It just does not run most of the time and is frozen until I ctrl+c it and restart it quite a few times. But I did get the solution function to start working correctly, following the rules of sudoku!
+
 
 #### What did I learn:
 * The random library is helpful for generating any type of random number for any case.
--- sudoku.py
+++ sudoku.py
@@ -8,6 +8,13 @@ def is_it_there(arr, value: int):
             boole = True
     return boole
 
+# box1 box2 box3
+# box4 box5 box6
+# box7 box8 box9
+
+# box1 box2
+# box3 box4
+
 def solution():
     rows = 3
     columns = 3
@@ -19,7 +26,12 @@ def solution():
     box2 = [0,0,0,0,0,0,0,0,0]
     box3 = [0,0,0,0,0,0,0,0,0]
     box4 = [0,0,0,0,0,0,0,0,0]
-
+    box5 = [0,0,0,0,0,0,0,0,0]
+    box6 = [0,0,0,0,0,0,0,0,0]
+    box7 = [0,0,0,0,0,0,0,0,0]
+    box8 = [0,0,0,0,0,0,0,0,0]
+    box9 = [0,0,0,0,0,0,0,0,0]
+    
     # initializing box2
     trackindex = 0
     for i,items in enumerate(box2):
@@ -36,37 +48,109 @@ def solution():
     trackindex = 0
     for i,items in enumerate(box3):
         num = random.randint(1,9)
-        while is_it_there(box3,num) == True or num in box1[:,trackindex]:
+        while is_it_there(box3,num) == True or num in box1[trackindex,:] or num in box2[trackindex,:]:
             num = random.randint(1,9)
         box3[i] = num
         if i == 2 or i == 5:
-            trackindex = 0
-        else:
             trackindex+=1
     box3 = np.array(box3)
     box3 = box3.reshape((3,3))
 
-    trackrows = 0
-    trackcols = 0
+    # initializing box4
+    trackindex = 0
     for i,items in enumerate(box4):
         num = random.randint(1,9)
-        while is_it_there(box4,num) == True or num in box2[:,trackcols] or num in box3[trackrows,:]:
+        while is_it_there(box4,num) == True or num in box1[:,trackindex]:
             num = random.randint(1,9)
         box4[i] = num
+        if i == 2 or i == 5:
+            trackindex = 0
+        else:
+            trackindex+=1
+    box4 = np.array(box4)
+    box4 = box4.reshape((3,3))
+
+    # initializing box5
+    trackrows = 0
+    trackcols = 0
+    for i,items in enumerate(box5):
+        num = random.randint(1,9)
+        while is_it_there(box5,num) == True or num in box2[:,trackcols] or num in box4[trackrows,:]:
+            num = random.randint(1,9)
+        box5[i] = num
         if i == 2 or i == 5:
             trackrows+= 1
             trackcols = 0
         else:
             trackcols+=1
-    box4 = np.array(box4)
-    box4 = box4.reshape((3,3))
+    box5 = np.array(box5)
+    box5 = box5.reshape((3,3))
 
-    print(np.hstack((box1, box2)))
-    print(np.hstack((box3,box4)))
-    print(box1)
-    print(box2)
-    print(box3)
-    print(box4)
+    # initializing box6
+    trackrows = 0
+    trackcols = 0
+    for i,items in enumerate(box6):
+        num = random.randint(1,9)
+        while is_it_there(box6,num) == True or num in box3[:,trackcols] or num in box4[trackrows,:] or num in box5[trackrows,:]:
+            num = random.randint(1,9)
+        box6[i] = num
+        if i == 2 or i == 5:
+            trackrows+= 1
+            trackcols = 0
+        else:
+            trackcols+=1
+    box6 = np.array(box6)
+    box6 = box6.reshape((3,3))
+
+    # initializing box7
+    trackindex = 0
+    for i,items in enumerate(box7):
+        num = random.randint(1,9)
+        while is_it_there(box7,num) == True or num in box1[:,trackindex] or num in box4[:,trackindex]:
+            num = random.randint(1,9)
+        box7[i] = num
+        if i == 2 or i == 5:
+            trackindex = 0
+        else:
+            trackindex+=1
+    box7 = np.array(box7)
+    box7 = box7.reshape((3,3))
+
+    # initializing box 8
+    trackrows = 0
+    trackcols = 0
+    for i,items in enumerate(box8):
+        num = random.randint(1,9)
+        while is_it_there(box8,num) == True or num in box2[:,trackcols] or num in box5[:,trackcols] or num in box7[trackrows,:]:
+            num = random.randint(1,9)
+        box8[i] = num
+        if i == 2 or i == 5:
+            trackrows+= 1
+            trackcols = 0
+        else:
+            trackcols+=1
+    box8 = np.array(box8)
+    box8 = box8.reshape((3,3))
+
+    # initializing box 9
+    trackrows = 0
+    trackcols = 0
+    for i,items in enumerate(box9):
+        num = random.randint(1,9)
+        while is_it_there(box9,num) == True or num in box3[:,trackcols] or num in box6[:,trackcols] or num in box7[trackrows,:] or num in box8[trackrows,:]:
+            num = random.randint(1,9)
+        box9[i] = num
+        if i == 2 or i == 5:
+            trackrows+= 1
+            trackcols = 0
+        else:
+            trackcols+=1
+    box9 = np.array(box9)
+    box9 = box9.reshape((3,3))
+
+    print(np.hstack((box1, box2,box3)))
+    print(np.hstack((box4,box5,box6)))
+    print(np.hstack((box7,box8,box9)))
 
 if __name__ == "__main__":
     solution()
