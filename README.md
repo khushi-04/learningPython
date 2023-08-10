96dbf7741835ce8c5ac071423c86ecf1b51ca444
Khushi Chaudhari <khushi.chaudhari@gmail.com>
2023-08-09 21:38:57 -0700

    working on debugging

--- README.md
+++ README.md
@@ -17,9 +17,9 @@ This is a sudoku game in progress. I included nine 3x3 boxes that all correspond
 
 8/7: I've been able to generate 4 boxes with random numbers that follow the sudoku rules so far. They are also formatted in the way that I want. Now I have to do it for the rest of the boxes, which is where it might get a little complicated.
 
-8/9: One of the frustrating things I've been dealing with is my code not running properly in the terminal. It just does not run most of the time and is frozen until I ctrl+c it and restart it quite a few times. But I did get the solution function to start working correctly, following the rules of sudoku!
-
+8/9: One of the frustrating things I've been dealing with is my code not running properly in the terminal. I'll have to spend some time debugging and finding help from the internet. But I did get the solution function to start working correctly, following the rules of sudoku! I also have a decent introduction in main. 
 
 #### What did I learn:
 * The random library is helpful for generating any type of random number for any case.
 * The NumPy library is very useful for manipulating and generating 2D and 3D arrays. It has a variety of functions that allow you to change the way an array looks and is structured a lot easier than just using lists. 
+* While researching, I learned that Python is actually a slow language compared to other language, so it's not suitable for incredibly complex code. But the reason it's so popular is because of its readability and ease of use. 
--- sudoku.py
+++ sudoku.py
@@ -1,19 +1,11 @@
-import random
+from random import randint
 import numpy as np
 
 def is_it_there(arr, value: int):
-    boole = False
     for item in arr:
         if value == item:
-            boole = True
-    return boole
-
-# box1 box2 box3
-# box4 box5 box6
-# box7 box8 box9
-
-# box1 box2
-# box3 box4
+            return True
+    return False
 
 def solution():
     rows = 3
@@ -34,123 +26,146 @@ def solution():
     
     # initializing box2
     trackindex = 0
-    for i,items in enumerate(box2):
-        num = random.randint(1,9)
+    box2 = np.array(box2)
+    for i in range(0,len(box2)):
+        num = randint(1,9)
         while is_it_there(box2,num) == True or num in box1[trackindex,:]:
-            num = random.randint(1,9)
-        box2[i] = num
+            num = randint(1,9)
         if i == 2 or i == 5:
             trackindex+=1
-    box2 = np.array(box2)
+        box2[i] = num
+    print(box2)
+    
     box2 = box2.reshape((3,3))
 
     # initializing box3
-    trackindex = 0
-    for i,items in enumerate(box3):
-        num = random.randint(1,9)
-        while is_it_there(box3,num) == True or num in box1[trackindex,:] or num in box2[trackindex,:]:
-            num = random.randint(1,9)
-        box3[i] = num
-        if i == 2 or i == 5:
-            trackindex+=1
+    # trackindex = 0
+    # for i,items in enumerate(box3):
+    #     num = randint(1,9)
+    #     while is_it_there(box3,num) == True or num in box1[trackindex,:] or num in box2[trackindex,:]:
+    #         num = randint(1,9)
+    #     box3[i] = num
+    #     if i == 2 or i == 5:
+    #         trackindex+=1
     box3 = np.array(box3)
     box3 = box3.reshape((3,3))
 
     # initializing box4
-    trackindex = 0
-    for i,items in enumerate(box4):
-        num = random.randint(1,9)
-        while is_it_there(box4,num) == True or num in box1[:,trackindex]:
-            num = random.randint(1,9)
-        box4[i] = num
-        if i == 2 or i == 5:
-            trackindex = 0
-        else:
-            trackindex+=1
+    # trackindex = 0
+    # for i,items in enumerate(box4):
+    #     num = randint(1,9)
+    #     while is_it_there(box4,num) == True or num in box1[:,trackindex]:
+    #         num = randint(1,9)
+    #     box4[i] = num
+    #     if i == 2 or i == 5:
+    #         trackindex = 0
+    #     else:
+    #         trackindex+=1
     box4 = np.array(box4)
     box4 = box4.reshape((3,3))
 
     # initializing box5
-    trackrows = 0
-    trackcols = 0
-    for i,items in enumerate(box5):
-        num = random.randint(1,9)
-        while is_it_there(box5,num) == True or num in box2[:,trackcols] or num in box4[trackrows,:]:
-            num = random.randint(1,9)
-        box5[i] = num
-        if i == 2 or i == 5:
-            trackrows+= 1
-            trackcols = 0
-        else:
-            trackcols+=1
+    # trackrows = 0
+    # trackcols = 0
+    # for i,items in enumerate(box5):
+    #     num = randint(1,9)
+    #     while is_it_there(box5,num) == True or num in box2[:,trackcols] or num in box4[trackrows,:]:
+    #         num = randint(1,9)
+    #     box5[i] = num
+    #     if i == 2 or i == 5:
+    #         trackrows+= 1
+    #         trackcols = 0
+    #     else:
+    #         trackcols+=1
     box5 = np.array(box5)
     box5 = box5.reshape((3,3))
 
     # initializing box6
-    trackrows = 0
-    trackcols = 0
-    for i,items in enumerate(box6):
-        num = random.randint(1,9)
-        while is_it_there(box6,num) == True or num in box3[:,trackcols] or num in box4[trackrows,:] or num in box5[trackrows,:]:
-            num = random.randint(1,9)
-        box6[i] = num
-        if i == 2 or i == 5:
-            trackrows+= 1
-            trackcols = 0
-        else:
-            trackcols+=1
+    # trackrows = 0
+    # trackcols = 0
+    # for i,items in enumerate(box6):
+    #     num = randint(1,9)
+    #     while is_it_there(box6,num) == True or num in box3[:,trackcols] or num in box4[trackrows,:] or num in box5[trackrows,:]:
+    #         num = randint(1,9)
+    #     box6[i] = num
+    #     if i == 2 or i == 5:
+    #         trackrows+= 1
+    #         trackcols = 0
+    #     else:
+    #         trackcols+=1
     box6 = np.array(box6)
     box6 = box6.reshape((3,3))
 
     # initializing box7
-    trackindex = 0
-    for i,items in enumerate(box7):
-        num = random.randint(1,9)
-        while is_it_there(box7,num) == True or num in box1[:,trackindex] or num in box4[:,trackindex]:
-            num = random.randint(1,9)
-        box7[i] = num
-        if i == 2 or i == 5:
-            trackindex = 0
-        else:
-            trackindex+=1
+    # trackindex = 0
+    # for i,items in enumerate(box7):
+    #     num = randint(1,9)
+    #     while is_it_there(box7,num) == True or num in box1[:,trackindex] or num in box4[:,trackindex]:
+    #         num = randint(1,9)
+    #     box7[i] = num
+    #     if i == 2 or i == 5:
+    #         trackindex = 0
+    #     else:
+    #         trackindex+=1
     box7 = np.array(box7)
     box7 = box7.reshape((3,3))
 
     # initializing box 8
-    trackrows = 0
-    trackcols = 0
-    for i,items in enumerate(box8):
-        num = random.randint(1,9)
-        while is_it_there(box8,num) == True or num in box2[:,trackcols] or num in box5[:,trackcols] or num in box7[trackrows,:]:
-            num = random.randint(1,9)
-        box8[i] = num
-        if i == 2 or i == 5:
-            trackrows+= 1
-            trackcols = 0
-        else:
-            trackcols+=1
+    # trackrows = 0
+    # trackcols = 0
+    # for i,items in enumerate(box8):
+    #     num = randint(1,9)
+    #     while is_it_there(box8,num) == True or num in box2[:,trackcols] or num in box5[:,trackcols] or num in box7[trackrows,:]:
+    #         num = randint(1,9)
+    #     box8[i] = num
+    #     if i == 2 or i == 5:
+    #         trackrows+= 1
+    #         trackcols = 0
+    #     else:
+    #         trackcols+=1
     box8 = np.array(box8)
     box8 = box8.reshape((3,3))
 
     # initializing box 9
-    trackrows = 0
-    trackcols = 0
-    for i,items in enumerate(box9):
-        num = random.randint(1,9)
-        while is_it_there(box9,num) == True or num in box3[:,trackcols] or num in box6[:,trackcols] or num in box7[trackrows,:] or num in box8[trackrows,:]:
-            num = random.randint(1,9)
-        box9[i] = num
-        if i == 2 or i == 5:
-            trackrows+= 1
-            trackcols = 0
-        else:
-            trackcols+=1
+    # trackrows = 0
+    # trackcols = 0
+    # for i,items in enumerate(box9):
+    #     num = randint(1,9)
+    #     while is_it_there(box9,num) == True or num in box3[:,trackcols] or num in box6[:,trackcols] or num in box7[trackrows,:] or num in box8[trackrows,:]:
+    #         num = randint(1,9)
+    #     box9[i] = num
+    #     if i == 2 or i == 5:
+    #         trackrows+= 1
+    #         trackcols = 0
+    #     else:
+    #         trackcols+=1
     box9 = np.array(box9)
     box9 = box9.reshape((3,3))
 
-    print(np.hstack((box1, box2,box3)))
-    print(np.hstack((box4,box5,box6)))
-    print(np.hstack((box7,box8,box9)))
+    row1 = np.hstack((box1, box2,box3))
+    row2 = np.hstack((box4,box5,box6))
+    row3 = np.hstack((box7,box8,box9))
+    stack1 = np.vstack((row1,row2))
+    main_stack = np.vstack((stack1,row3))
+    return main_stack
+
+def new_game():
+    main_stack = solution()
+    return ""
 
 if __name__ == "__main__":
-    solution()
+
+    print("Hello, welcome to Sudoku! In this game, I will give you a sudoku grid (9x9).")
+    cont = input("Type 'Y' to begin and 'N' to quit: ")
+    while cont != "N" and cont != "n":
+        print("Instructions:")
+        print("To input an answer to an empty slot, the game will first ask for which slot (type 'row column' from 1-9 for each). Then you can type in your answer!")
+        print("You get three lives, and if you guess incorrectly three times, the game is over. You can always play another game!")
+        print(" ")
+        new_game()
+
+        
+        cont = input("Type 'Y' to play again and 'N' to quit: ")
+
+    print("Thank you for playing. Have a good day!")
+    # solution()
